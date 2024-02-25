f2150c9867be4dd1ba491e661a7f19f88532eee3
rana <rana@gmail.com>
2024-02-25 20:08:13 +0000

    added some files

--- test.sh
+++ test.sh
@@ -5,4 +5,5 @@ docker build -t newimage .
 docker tag newimage ded1327/mynewimage1
 echo "$Password" | docker login --username "$USERNAME" --password-stdin
 docker push ded1327/mynewimage1
-
+sleep 10
+rsync -avh /var/lib/jenkins/workspace/Newtestjob/*.yml  root@172.31.32.149:/tmp/alok
